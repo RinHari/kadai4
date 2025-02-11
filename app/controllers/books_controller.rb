ecc5a86db609d54bfde015219ab9c76fd1cff1f1
EC2 Default User <ec2-user@ip-172-31-15-219.ap-northeast-1.compute.internal>
2025-02-11 03:55:25 +0000

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  def index
+    @books = Book.all
+    @new_book = Book.new
+    
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  # 投稿データの保存
+  def create
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@new_book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])  # Bookを取得
+    unless book.user == current_user
+      redirect_to books_path
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :create, :edit, :show, :update, :destroy]
+
+  get 'about', to: 'homes#about', as: 'about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
